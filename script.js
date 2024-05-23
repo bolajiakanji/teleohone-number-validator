7818b4e5b9c2e83d9f76247d3d52cf889fd02666
lukman bolaji <bolajiakanji21@gmail.com>
2024-05-22 22:43:20 -0700

    add javascript

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 let userInput = document.getElementById("user-input").value
-let checkButton = document.getElementById("user-input").value
-let clearButton = document.getElementById("user-input").value
-le
-function telephoneCheck(str) {
-    let a=/^(1\s?)?(\(\d{3}\)\s?\d{3}-|\d{3}\s?\d{3}\s?|\d{3}-\d{3}-)\d{4}$/g
-    return a.test(str);
+let checkBtn = document.getElementById("check-btn")
+let clearBtn = document.getElementById("clear-btn")
+let output = document.getElementById("output").innerHTML
+
+function validitate(input) {
+    let regex=/^(1\s?)?(\(\d{3}\)\s?\d{3}-|\d{3}\s?\d{3}\s?|\d{3}-\d{3}-)\d{4}$/g
+    return regex.test(input);
   }
   
-function checkButton() {
+function numberValidity() {
     if (userInput === "") {
      alert("please provide a phone number")
+    } else {
+        if (validitate(userInput)) {
+            output = `Invalid US number: ${userInput}`
+        } else {
+            output = `Valid US number: ${userInput}`
+        }
     }
+    userInput=""
+ }
+
+ function clear() {
+    userInput=""
  }
